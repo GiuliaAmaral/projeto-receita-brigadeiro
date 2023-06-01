11d43a2f879bcae00525fe9048f89d92d0142c50
Giulia Amaral <75877954+GiuliaAmaral@users.noreply.github.com>
2023-06-01 17:03:07 -0300

    adicionada função de imprimir

---
+++ script.js
@@ -0,0 +1,11 @@
+function imprimir(){
+    window.print();
+};
+
+// function imprimirParteEspecifica(){
+//     var conteudo = document.getElementById("nome-do-id-ou-classe").innerHTML;
+//     var body = document.body.innerHTML;
+//     document.body.innerHTML = conteudo;
+//     window.print();
+//     document.body.innerHTML = body;
+// }
